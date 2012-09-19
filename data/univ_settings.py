5f73f83e9bb070414408a70a48405af09e02354f
Nathaniel Gaertner <gaertner@gaertner-lap.(none)>
2012-09-19 18:06:38 -0400

    minor

--- data/univ_settings.py
+++ data/univ_settings.py
@@ -3,7 +3,7 @@ table_functions = imp.load_module('reformat', *imp.find_module('reformat', ['dat
 DATABASE_CONFIG = {
         'user':'postgres',
         'db':'bip3',
-        'pw':'|-|3lp3rb34r'
+        'pw':'postgrespw'
         }
 ERSATZPG_CONFIG = {
         'debug':True
